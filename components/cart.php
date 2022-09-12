e25462bd7b3de4883ac2ca877472ba02712137ce
Đoàn Trần Bá Đạt <89367921+doandat8888@users.noreply.github.com>
2022-09-12 09:58:49 +0700

    phân chia lại component theo php

---
+++ components/cart.php
@@ -0,0 +1,24 @@
+<div class="col-lg-4 col-md-4 col-9 cart">
+    <div class="cart-header">
+        <div class="cart-title">
+            <div class="content">Giỏ hàng</div>
+            <div class="quantity"></div>
+        </div>
+        <i class="fa-solid fa-xmark cancel-icon"></i>
+    </div>
+    <div class="cart-products">
+        <?php 
+            include_once "cartProduct.php";
+        ?>
+    </div>
+    <div class="cart-total">
+        <div class="cart-total-title">Tổng cộng:</div>
+        <div class="cart-total-money"></div>
+    </div>
+    <a href="../../pages/cart/index.php" class="cart-btn-link">
+        <div class="cart-btn-view">
+            Xem giỏ hàng
+        </div>
+    </a>
+
+</div>
---
+++ components/header.php
@@ -0,0 +1,69 @@
+<div class="search-section d-flex justify-content-center align-items-center">
+    <input type="text" placeholder="TÌM KIẾM SẢN PHẨM" class="search-input">
+    <a href="../pages/search/index.php" class="search-link" style="color: none !important;">
+        <span class="material-symbols-outlined header-icon search-icon-1">
+            search
+        </span>
+    </a>
+
+    <i class="fa-solid fa-xmark cancel-icon"></i>
+</div>
+<div class="category">
+    <i class="fa-solid fa-xmark cancel-icon"></i>
+    <br>
+    <a href="./pages/login/index.php">
+        <button class="login-btn">
+            Đăng nhập
+        </button>
+    </a>
+    <ul class="category-list">
+        <li class="category-item">Trang chủ</li>
+        <li class="category-item">Sản phẩm</li>
+        <li class="category-item">Nam</li>
+        <li class="category-item">Nữ</li>
+        <li class="category-item">Liên hệ</li>
+    </ul>
+</div>
+
+
+
+<div id="header">
+    <div class="container">
+        <div class="logo">
+            <a href="../../index.php"><img src="../../src/img/logo.png" class="image" /></a>
+        </div>
+        <div id="navigation-bar">
+            <span class="material-symbols-outlined bar-icon">
+                menu
+            </span>
+        </div>
+        <div class="list d-md-none d-lg-flex align-items-center justify-content-center">
+            <ul class="nav-list hide-on-mobile-tablet">
+                <li class="nav-item">Trang chủ</li>
+                <li class="nav-item">Sản phẩm</li>
+                <li class="nav-item">Nam</li>
+                <li class="nav-item">Nữ</li>
+                <li class="nav-item">Liên hệ</li>
+            </ul>
+        </div>
+        <div class="nav-icon">
+            <span class="material-symbols-outlined header-icon search-icon">
+                search
+            </span>
+            <div class="header-icon">
+                <span class="material-symbols-outlined cart-icon">
+                    shopping_cart
+                </span>
+                <span class="cart-number"></span>
+            </div>
+            <a href="./pages/login/index.php" class="header-icon user-icon">
+                <span class="material-symbols-outlined">
+                    person
+                </span>
+            </a>
+        </div>
+        <?php
+        include_once "cart.php";
+        ?>
+    </div>
+</div>
