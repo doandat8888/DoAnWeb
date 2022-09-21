922960e18db45eef2e04cb21e50260336514d5aa
Đoàn Trần Bá Đạt <89367921+doandat8888@users.noreply.github.com>
2022-09-21 14:29:55 +0700

    sdfsdf

--- pages/admin/manage-product.php
+++ pages/admin/manage-product.php
@@ -1,3 +1,46 @@
-<?php 
-    echo "This is manage product page!";
+<?php
+
 ?>
+<div class="manage-product">
+    <div class="title">Quản lí sản phẩm</div>
+    <div class="manage-product-body">
+        <table class="manage-product-table table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col-1">STT</th>
+                    <th scope="col-2">Hình ảnh</th>
+                    <th scope="col-4">Tên sản phẩm</th>
+                    <th scope="col-4">Giá</th>
+                    <th scope="col-4">Hành động</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td class="product-img-container col-2"><img src="../../src/img/products/women/product-women-1-1 (3).jpg" class="manage-product-img col-lg-6 col-12"></td>
+                    <td>Đầm lụa cố đô</td>
+                    <td>1.250.000VNĐ</td>
+                    <td class="manage-product-action">
+                        <button class="edit action-btn">
+                            Sửa
+                        </button>
+                        <button class="delete action-btn">Xóa</button>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td class="product-img-container">Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td class="product-img-container">Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</div>
