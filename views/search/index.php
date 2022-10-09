b94ccf1c27ae88f398cfe03ec3f79e6540f4e804
gitthebread <112457612+gitthebread@users.noreply.github.com>
2022-10-09 12:27:32 +0700

    search fix

--- views/search/index.php
+++ views/search/index.php
@@ -1,3 +1,37 @@
+<?php
+    session_start();
+    if (!function_exists('currency_format')) {
+        function currency_format($number, $suffix = 'đ') {
+            if (!empty($number)) {
+                return number_format($number, 0, ',', '.') . "{$suffix}";
+            }
+        }
+    }
+    if (!function_exists('color_format')) {
+        function color_format($color) {
+            $colorHex = "";
+            $arraycolor = array(
+                "blue" => "C6E9EC",
+                "white" => "FFFFFF",
+                "pink" => "FB6E7C",
+                "orange" => "F3A45F",
+                "yellow" => "F4ED95",
+                "red" => "EC3333",
+                "black" => "212529",
+                "green" => "98A882",
+                "gray" => "A8A9AD",
+            );
+            while($element = current($arraycolor)) {
+                if(key($arraycolor) == $color) {
+                    $colorHex = $arraycolor[key($arraycolor)];
+                }
+                next($arraycolor);
+            }
+            return $colorHex;
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -114,64 +148,78 @@
                         $searchstr = $_GET["searchstr"];
                         $data = $controller->getProductByName($searchstr);
 
-                        foreach($data as $product){
-                            echo "
-                            
-                            <div class='col-lg-3 col-md-6 col-6 product-search-result'>
-                                <div class='card'>
-                                    <div class='product-img'>
-                                        <span class='badget'>
-                                            -50%
-                                        </span>
-                                        <img src='".$product->getImage02()."' class='product-img-content product-img2'/>
-                                        <img src='".$product->getImage01()."' class='product-img-content product-img1'/>
-                                        <div class='pro-btn d-flex'>
-                                            <a href='#' class='hidden-btn'>
-                                                <i class='fa-solid fa-eye'></i>
+                        if (empty($data)){
+                            echo "<h1 style='margin-left:1rem;'>Không tìm thấy kết quả nào</h1>";
+                        }
+                        else{
+                            foreach($data as $product){
+                                $arraycolor = explode(", ",$product->getColor());
+                                echo "  
+                                <div class='col-lg-3 col-md-6 col-6 product-search-result'>
+                                    <div class='card'>
+                                        <div class='product-img'>
+                                            <span class='badget'>
+                                                -50%
+                                            </span>
+                                            <a href='../../views/detailProduct/indexpd.php?id=".$product->getId()."'>
+                                                <img src='".$product->getImage02()."' class='product-img-content product-img-2'/>
+                                                <img src='".$product->getImage01()."' class='product-img-content product-img-1'/>
+                                            </a>
+                                            <div class='pro-btn d-flex'>
+                                                <a href='#' class='hidden-btn'>
+                                                    <i class='fa-solid fa-eye'></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class='card-body'>
+                                            <h5 class='card-title product-info'>
+                                                <div class='list-color d-flex'>
+                                                    <div class='dot-list d-flex'>
+                                                    ";?>
+                                                        <?php 
+                                                            foreach ($arraycolor as $cpro){
+                                                                $colorHex = color_format($cpro);
+                                                                echo '<label class="color-button" style="background-color:#'.$colorHex.';" for="'.strtolower($cpro).'"></label>'; 
+                                                            }
+                                                        ?>
+                                                    <?php
+                                                    echo "
+                                                    </div>
+                                                    <div class='favorite'>
+                                                        <span class='material-symbols-outlined favorite-icon'>
+                                                            favorite
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <div class='product-name'>
+                                                    ".$product->getName()."
+                                                </div>
+                                            </h5>
+                                            <p class='card-text'>
+                                                <div class='product-price d-flex'>
+                                                    <div class='product-price__new'>".currency_format($product->getPrice())."</div>
+                                                    <strike><div class='product-price__old'>1.150.000đ</div></strike>
+                                                </div>
+                                            </p>
+                                            <a href='#' class='btn btn-primary' style='background-color: transparent; border: none;'>
+                                                <div class='product-cart'>
+                                                    <span class='material-symbols-outlined product-cart-icon'>
+                                                        local_mall
+                                                    </span>
+                                                    <p class='product-cart-buy'>Mua ngay</p>
+                                                </div>
                                             </a>
                                         </div>
                                     </div>
-                                    <div class='card-body'>
-                                        <h5 class='card-title product-info'>
-                                            <div class='list-color d-flex'>
-                                                <div class='dot-list d-flex'>
-                                                    <div class='dot green'></div>
-                                                    <div class='dot pink'></div>
-                                                    <div class='dot yellow'></div>
-                                                </div>
-                                                <div class='favorite'>
-                                                    <span class='material-symbols-outlined favorite-icon'>
-                                                        favorite
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <div class='product-name'>
-                                                ".$product->getName()."
-                                            </div>
-                                        </h5>
-                                        <p class='card-text'>
-                                            <div class='product-price d-flex'>
-                                                <div class='product-price__new'>".$product->getPrice()."</div>
-                                                <strike><div class='product-price__old'>1.150.000đ</div></strike>
-                                            </div>
-                                        </p>
-                                        <a href='#' class='btn btn-primary' style='background-color: transparent; border: none;'>
-                                            <div class='product-cart'>
-                                                <span class='material-symbols-outlined product-cart-icon'>
-                                                    local_mall
-                                                </span>
-                                                <p class='product-cart-buy'>Mua ngay</p>
-                                            </div>
-                                        </a>
-                                    </div>
                                 </div>
-                            </div>
-                            ";
+                                ";
+                            }
                         }
                     }
                     else{
-                        echo "No results found";
-                    }    
+                        echo "Không tìm thấy kết quả nào";
+                    }
+                    
                 ?>
             </div>
         </div>
