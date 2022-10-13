5b9040d7c9e185f451b30b90e4bb7ad3cdcc6ebc
gitthebread <112457612+gitthebread@users.noreply.github.com>
2022-10-13 17:44:09 +0700

    cart semi-fixed

--- controllers/cartController.php
+++ controllers/cartController.php
@@ -5,9 +5,7 @@
         switch($_POST['action']){
             case 'add':
                 $item = array('id'=>$_POST['product_id'], 'name'=>$_POST['product_name'], 'price'=>$_POST['product_price'], 'image'=>$_POST['product_img'], 'image'=>$_POST['product_color'], 'quantity'=>1);
-                // $item = array(
-                //     'id'=>$_POST['product_id']
-                // ); 
+                //unset($_SESSION['cart']);
                 addToCart($item);
                 break;
             case 'update':
--- modules/cart_module.php
+++ modules/cart_module.php
@@ -1,12 +1,17 @@
 <?php
     function addToCart($item){
         if (isset($_SESSION['cart'])){
-            // $cart = $_SESSION['cart'];
-            // if (!array_key_exists($cart["product_id"], $cart)){
-            //     $cart[$item["product_id"]] = $item;
-            // }
-            // $_SESSION['cart'] = $cart;
-            print_r($_SESSION['cart']);
+            if(array_key_exists($item['id'], $_SESSION['cart'])){
+                // echo "<script>alert('Sản phẩm này đã có trong giỏ hàng')</script>";
+                // echo "<script>window.location = 'index.php'</script>";
+                echo "<h1 style='margin-left:1rem;'>Item in cart</h1>";
+                print_r($_SESSION['cart']);
+            }
+            else{
+                $count = count($_SESSION['cart']);
+                $_SESSION['cart'][$count] = $item;
+                print_r($_SESSION['cart']);
+            }
         }
         else{
             $_SESSION['cart'][0] = $item;
